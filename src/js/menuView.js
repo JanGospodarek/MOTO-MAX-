ac1e4af82cc6e692d8d1882de22646d305659cfd
JanGospodarek <jan.gospo@onet.pl>
2022-05-22 00:19:55 +0200

    dropMenus

--- src/js/menuView.js
+++ src/js/menuView.js
@@ -1,11 +1,34 @@
 import { View } from "./View.js";
 class MenuView extends View {
   window = document.querySelector(".contactCont");
+  parentElement = document.querySelector(".menu");
   btnOpen = document.querySelector("#btnContact");
+  categories = document.querySelectorAll(".category");
   constructor() {
     super();
     this.addHandlerShowWindow();
     this.addHandlerHideWindow();
+    this.handlerDropMenu("akcesoria");
+    this.handlerDropMenu("czesci");
+    this.handlerDropMenu("ubrania");
+  }
+
+  handlerDropMenu(element) {
+    const btn = document.querySelector(`.${element}`);
+    const dropMenu = document.querySelector(`#${element}`);
+    function helper() {
+      if (element === "akcesoria") dropMenu.style.height = "120px";
+      if (element === "czesci") dropMenu.style.height = "60px";
+      if (element === "ubrania") dropMenu.style.height = "120px";
+    }
+    btn.addEventListener("mouseover", helper);
+    btn.addEventListener("mouseleave", () => {
+      dropMenu.style.height = "0";
+    });
+    dropMenu.addEventListener("mouseleave", () => {
+      dropMenu.style.height = "0";
+    });
+    dropMenu.addEventListener("mouseover", helper);
   }
 }
 export const menuView = new MenuView();
